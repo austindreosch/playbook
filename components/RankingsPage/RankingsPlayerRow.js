7a08789d490be55b28abfccd1b378cc3afdb7f15
Austin Dreosch <austindreosch@gmail.com>
2025-04-11 04:52:47 -0700

    collapsed playerrow styling basics are done

--- components/RankingsPage/RankingsPlayerRow.js
+++ components/RankingsPage/RankingsPlayerRow.js
@@ -3,6 +3,10 @@
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import React, { memo, useEffect, useRef, useState } from 'react';
+import BullseyeIcon from '../icons/BullseyeIcon';
+import CalendarIcon from '../icons/CalendarIcon';
+import FlagIcon from '../icons/FlagIcon';
+import { PeopleGroupIcon } from '../icons/PeopleGroupIcon';
 
 // Create a specialized component just for stats to reduce re-renders
 const StatsSection = memo(({ categories, stats }) => {
@@ -28,6 +32,38 @@ const StatsSection = memo(({ categories, stats }) => {
 });
 
 StatsSection.displayName = 'StatsSection';
+
+// Secondary stats section for last 30 etc
+const StatsSectionSecondary = memo(({ categories, stats }) => {
+    return (
+        <div className="flex w-full h-full gap-[3px]">
+            {categories.map((statKey) => {
+                const stat = stats[statKey];
+                return (
+                    <div
+                        key={statKey}
+                        className="flex-1 text-center h-full flex flex-col items-center justify-center select-none bg-gray-200 border border-gray-300 rounded-sm relative shadow-sm"
+                        title={`${stat?.abbreviation || statKey}: ${stat?.value}`}
+                    >
+                        <div className="text-sm text-pb_darkgray z-10">
+                            <div className="flex items-center justify-center w-full pb-2.5">
+                                {stat?.value === 0 ? '0' : stat?.value?.toFixed(1)}
+                            </div>
+
+                        </div>
+                        <div style={{ backgroundColor: stat?.color }} className="absolute bottom-0 w-full h-2.5 border-t border-gray-300">
+                        </div>
+                    </div>
+
+                );
+            })}
+        </div >
+    );
+});
+
+StatsSectionSecondary.displayName = 'StatsSectionSecondary';
+
+
 
 const RankingsPlayerRow = memo(({ player, sport, categories, rank, isExpanded, onExpand }) => {
     const rowRef = useRef(null);
@@ -159,31 +195,92 @@ const RankingsPlayerRow = memo(({ player, sport, categories, rank, isExpanded, o
 
             {/* Only render expanded content when needed */}
             {isExpanded && (
-                <div className="flex flex-col w-full h-[180px] shadow-[inset_0_2px_4px_rgba(0,0,0,0.1)]">
+                <div className="flex flex-col w-full h-[180px] border-t border-gray-300 shadow-[inset_0_2px_4px_rgba(0,0,0,0.8)]">
 
                     {/* inner */}
-                    <div className="flex h-24 h-full">
-                        {/* Left orange panel */}
-                        <div className="bg-orange-400 w-[10%] items-center justify-center">
-                            <span className="font-bold">RANKS</span>
+                    <div className="flex h-24 h-full bg-gray-100">
+                        {/* Left panel */}
+                        <div className=" w-[9%] items-center justify-center ml-auto flex flex-col gap-4 ">
+                            <div className='flex flex-col items-center'>
+                                <div className='bg-white h-11 w-16 border border-gray-300 rounded-sm flex items-center justify-center'>
+                                    <span className="font-bold text-lg">15</span>
+                                </div>
+                                <span className='text-xs tracking-wider mt-1 text-pb_darkgray'>STANDARD</span>
+                            </div>
+                            <div className='flex flex-col items-center'>
+                                <div className='bg-white h-11 w-16 border border-gray-300 rounded-sm flex items-center justify-center'>
+                                    <span className="font-bold text-lg">13</span>
+                                </div>
+                                <span className='text-xs tracking-wider mt-1 text-pb_darkgray'>REDRAFT</span>
+                            </div>
                         </div>
-                        <div className=" w-[30%]">
-                            <div className="bg-blue-300 flex items-center justify-center h-1/2">
-                                <span className="font-bold">INSIGHTS PANEL</span>
+
+                        {/* middle panel */}
+                        <div className=" w-[24%] pr-3">
+                            {/* Insights panel */}
+                            <div className="flex flex-col justify-center items-center pt-2.5 h-[50%] px-3">
+                                <div className="w-full h-11 flex relative overflow-hidden rounded-sm mx-2">
+                                    <div className="bg-pb_orange h-full w-[65%]"></div>
+                                    <div className="bg-pb_blue h-full flex-1"></div>
+                                    <div className="absolute inset-0 flex items-center justify-center">
+                                        <div className="bg-gray-700 text-white font-bold py-1 px-3 rounded">
+                                            95
+                                        </div>
+                                    </div>
+                                </div>
+                                <span className="text-xs tracking-wider mt-1 text-pb_darkgray">PLAYBOOK SCORE</span>
                             </div>
 
-                            <div className="flex h-1/2 items-center justify-center bg-orange-200">
-                                <span className="font-bold">STATIC INSIGHTS</span>
-
+                            <div className="flex h-[50%] items-center justify-center pb-2">
+                                <div className="flex-1 flex flex-col items-center justify-center">
+                                    {/* <span className="text-xs tracking-wider mb-2">CLE</span> */}
+                                    <div className='bg-pb_green h-4 w-8 mb-3'></div>
+                                    <FlagIcon className="w-5 h-5" />
+                                </div>
+                                <div className="flex-1 flex flex-col items-center justify-center">
+                                    <span className="text-xs tracking-wider mb-3">CLE</span>
+                                    <PeopleGroupIcon className="w-5 h-5" />
+                                </div>
+                                <div className="flex-1 flex flex-col items-center justify-center">
+                                    <span className="text-xs tracking-wider mb-3">28</span>
+                                    <CalendarIcon className="w-5 h-5" />
+                                </div>
+                                <div className="flex-1 flex flex-col items-center justify-center">
+                                    <span className="text-xs tracking-wider mb-3">SG,SF</span>
+                                    <BullseyeIcon className="w-5 h-5" />
+                                </div>
                             </div>
                         </div>
-                        <div className="bg-red-300 w-[60%]">
-                            <div className="bg-green-300 p-2 flex items-center justify-center h-[30%]">
-                                <span className="font-bold">LAST 30</span>
+
+                        {/* right panel 0*/}
+                        <div className="w-[7%] items-center justify-center border-l border-pb_lightgray">
+                            <div className="p-2 flex flex-col h-[30%] items-center justify-center">
+                                <span className="text-xs tracking-wider">LAST 30</span>
+                                <span className="text-xs tracking-wider text-pb_green">▲ 47%</span>
+                            </div>
+
+                            <div className="flex h-[70%] items-center justify-center ">
+                                <div className="flex items-center justify-center">
+                                    <div className="flex flex-col gap-1">
+                                        <button className="text-xs tracking-wider bg-gray-600 text-white hover:bg-gray-500 hover:text-gray-100 px-2 py-2 rounded shadow-sm transition-colors">
+                                            TIPS
+                                        </button>
+                                        <button className="text-xs tracking-wider bg-gray-200 hover:bg-gray-300 px-2 py-2 rounded shadow-sm transition-colors border border-gray-300">
+                                            MATCH
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* right panel 1*/}
+                        <div className="w-[60%]">
+                            <div className="py-2 flex items-center justify-center h-[30%]">
+                                <StatsSectionSecondary categories={categories} stats={player.stats} />
                             </div>
 
                             <div className="flex h-[70%] items-center justify-center">
-                                <span className="font-bold">DETAIL PANEL</span>
+                                <span className="bg-white font-bold h-full w-full border border-gray-300 rounded-sm mr-1 mb-1.5"></span>
 
                             </div>
                         </div>
