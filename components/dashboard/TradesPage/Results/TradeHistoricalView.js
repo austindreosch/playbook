d02b8b9ed10b95f20c95dede5cdfbb2345e7cf8d
Austin Dreosch <austindreosch@gmail.com>
2025-06-30 01:09:24 -0700

    1

--- components/dashboard/TradesPage/Results/TradeHistoricalView.js
+++ components/dashboard/TradesPage/Results/TradeHistoricalView.js
@@ -101,6 +101,7 @@ export default function TradeHistoricalView() {
                 />
               );
             }}
+            activeDot={false}
           />
           
           {/* Gradient definition */}
